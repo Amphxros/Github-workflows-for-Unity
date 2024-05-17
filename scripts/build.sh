a4e3c119502515147b939990fa19f58906f12d73
A. Rubio <ampxrubio@gmail.com>
2024-05-17 18:05:07 +0100

    deploy 8
    Co-Authored-By: Jorge Zurdo <18735746+Jorzuiz@users.noreply.github.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Configura las variables necesarias
-PROJECT_PATH=$(pwd)
+PROJECT_PATH=./project
 BUILD_PATH=$PROJECT_PATH/Build
 
 # Ejecuta la construcción en modo batch usando la imagen de GameCI
 docker run --rm \
     -v $PROJECT_PATH:/project \
     -v $BUILD_PATH:/build \
-    gameci/unity3d:2020.3.11f1 \
+    unityci/editor:ubuntu-2022.3.29f1-windows-mono-3 \
     -projectPath /project \
     -buildTarget StandaloneLinux64 \
     -quit \
